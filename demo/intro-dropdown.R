09c9d4648e4410914d8e6d460f2e328748c2d92e
Carson Sievert <cpsievert1@gmail.com>
2017-10-10 16:29:33 -0500

    more demo cleanup

---
+++ demo/intro-dropdown.R
@@ -0,0 +1,40 @@
+library(dasher)
+
+app <- Dash$new()
+
+# One way to include CSS to improve the sytle/look of your app is
+# to use html_link() to link to a stylesheet. Here we use the dash
+# styleguide https://codepen.io/chriddyp/pen/bWLwgP
+app$layout_set(
+  html_link(href = "https://codepen.io/chriddyp/pen/bWLwgP.css", rel = "stylesheet"),
+  core_input(id = 'inputID', value = 'initial value', type = "text"),
+  html_div(id = 'outputID')
+)
+
+app$callback(
+  function(x = input("inputID"), y = input("inputID", "type")) {
+    sprintf("You've entered: '%s' into the '%s' input control", x, y)
+  },
+  output("outputID")
+)
+
+app$run_server(showcase = TRUE)
+
+
+# For better performance, consider placing CSS in the <head> of the HTML
+# document (via app$dependencies_set(), which automatically places css in <head>)
+dash_css <- htmltools::htmlDependency(
+  name = "dash-css",
+  version = "1.0.0",
+  src = c(href = "https://codepen.io/chriddyp/pen"),
+  stylesheet = "bWLwgP.css"
+)
+
+app$layout_set(
+  core_input(id = 'inputID', value = 'initial value', type = "text"),
+  html_div(id = 'outputID')
+)
+
+app$dependencies_set(dash_css)
+app$serve_locally <- FALSE
+app$run_server(showcase = TRUE)
---
+++ demo/intro-htmlwidgets-plotly.R
@@ -0,0 +1,33 @@
+library(dasher)
+library(plotly)
+
+app <- Dash$new()
+
+nms <- names(mtcars)
+
+app$layout_set(
+  core_dropdown(
+    id = "x", options = nms, value = nms[1],
+    clearable = FALSE, placeholder = "Pick an x"
+  ),
+  core_dropdown(
+    id = "y", options = nms, value = nms[2],
+    clearable = FALSE, placeholder = "Pick a y"
+  ),
+  htmlwidget(id = 'plot', name = "plotly")
+)
+
+app$callback(
+  function(x = input("x"), y = input("y")) {
+    plot_ly(x = mtcars[[x]], y = mtcars[[y]])
+  },
+  # TODO: change x to value!?
+  output(id = 'plot', property = "x")
+)
+
+# TODO: better way to incorporate print time dependencies?
+app$dependencies_get()
+app$dependencies_set(plotly::plot_ly()$dependencies)
+app$dependencies_get()
+
+app$run_server(showcase = TRUE)
