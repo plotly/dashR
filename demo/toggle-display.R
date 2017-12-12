0f2dbed73e34c1c86a74e6e1fe0e3ea5daddd5e6
Carson Sievert <cpsievert1@gmail.com>
2017-12-12 16:27:03 -0600

    a demo of toggling display; fixes #9; thanks @chriddyp

---
+++ demo/toggle-display.R
@@ -0,0 +1,56 @@
+# Three ways to show/hide content via https://community.plot.ly/t/collapsible-div-in-dash/6634/4
+
+library(dasher)
+
+app <- Dash$new()
+
+app$layout_set(
+  coreRadioItems(
+    id = 'toggle',
+    options = lapply(c("Show", "Hide"), function(i) list(label = i, value = i)),
+    value = 'Hide'
+  ),
+  htmlDiv(
+    id = 'radio_container', style = list(display = "none"),
+    htmlImg(src = "https://www.r-project.org/logo/Rlogo.png", width = "100")
+  ),
+  htmlButton('Show', id = 'button'),
+  htmlDiv(
+    id = 'button_container', style = list(display = "none"),
+    htmlImg(src = "https://www.r-project.org/logo/Rlogo.png", width = "100")
+  ),
+  htmlDetails(
+    htmlSummary('Show / Hide'),
+    htmlImg(src = "https://www.r-project.org/logo/Rlogo.png", width = "100")
+  )
+)
+
+# show/hide on radio button toggle
+app$callback(
+  function(toggle = input("toggle")) {
+    display <- if (identical("Show", toggle)) "block" else "none"
+    list(display = display)
+  },
+  output("radio_container", "style")
+)
+
+# show/hide upon clicking button
+app$callback(
+  function(n = input("button", "n_clicks")) {
+    display <- if (isTRUE(n %% 2 == 1)) "block" else "none"
+    list(display = display)
+  },
+  output("button_container", "style")
+)
+
+app$callback(
+  function(n = input("button", "n_clicks")) {
+    if (isTRUE(n %% 2 == 1)) "Hide" else "Show"
+  },
+  output("button")
+)
+
+app$run_server(showcase = TRUE)
+
+
+
