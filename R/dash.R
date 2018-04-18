8e23775ab11dda901cf37f4beb4125aa93c79b34
Carson Sievert <cpsievert1@gmail.com>
2018-04-18 17:51:19 -0500

    use wildcard for index page route

--- R/dash.R
+++ R/dash.R
@@ -290,7 +290,8 @@ Dash <- R6::R6Class(
       })
 
 
-      route$add_handler("get", routes_pathname_prefix, function(request, response, keys, ...) {
+      index <- paste0(routes_pathname_prefix, "*")
+      route$add_handler("get", index, function(request, response, keys, ...) {
         response$body <- private$.index
         response$status <- 200L
         response$type <- 'html'
