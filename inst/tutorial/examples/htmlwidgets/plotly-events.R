2bbe3e1a2bf2615626f133f824901024e4c32925
Carson Sievert <cpsievert1@gmail.com>
2018-04-26 15:01:24 -0500

    example of the new dashHtmlwidgetComponent::Plotly

---
+++ inst/tutorial/examples/htmlwidgets/plotly-events.R
@@ -0,0 +1,42 @@
+library(dashR)
+library(plotly)
+library(dashHtmlwidgetComponent)
+
+p <- plot_ly(mtcars, x = ~wt, y = ~mpg) %>%
+  add_markers() %>%
+  layout(title = "Working with plotly htmlwidget events in dashR")
+
+app <- Dash$new()
+app$layout_set(
+  Plotly(id = 'plotID', widget = p, clear_on_unhover = TRUE, clear_on_doubleclick = TRUE),
+  htmlDiv(id = 'hover-data'),
+  htmlDiv(id = 'click-data'),
+  htmlDiv(id = 'selected-data'),
+  htmlDiv(id = 'relayout-data')
+)
+
+app$callback_print(
+  function(hoverData = input('plotID', 'hoverData')) {
+    if (!length(hoverData)) "Hover data will appear here" else hoverData
+  }, output('hover-data')
+)
+
+app$callback_print(
+  function(clickData = input('plotID', 'clickData')) {
+    if (!length(clickData)) "Click data will appear here" else clickData
+  }, output('click-data')
+)
+
+app$callback_print(
+  function(selectedData = input('plotID', 'selectedData')) {
+    if (!length(selectedData)) "Select data will appear here" else selectedData
+  }, output('selected-data')
+)
+
+app$callback_print(
+  function(relayoutData = input('plotID', 'relayoutData')) {
+    if (!length(relayoutData)) "Re-layout data will appear here" else relayoutData
+  }, output('relayout-data')
+)
+
+app$run_server()
