8cf3f3f93273b94e1107745d2a134cf6325d5ed1
Carson Sievert <cpsievert1@gmail.com>
2018-04-27 15:47:42 -0500

    leverage modern dashRwidgets; leaflet inputs example

--- inst/tutorial/examples/htmlwidgets/plotly-inputs.R
+++ inst/tutorial/examples/htmlwidgets/plotly-inputs.R
@@ -1,10 +1,11 @@
+# devtools::install_github('ropensci/plotly@setProps')
 library(dashR)
 library(dashRwidgets)
 library(plotly)
 
-app <- Dash$new()
-
 p <- plot_ly(mtcars, x = ~wt, y = ~mpg, key = row.names(mtcars))
+
+app <- Dash$new()
 
 app$layout_set(
   htmlwidget(id = 'plotID', widget = p),
@@ -16,28 +17,28 @@ app$layout_set(
 
 app$callback_print(
   function(hover = input("plotID", "input_plotly_hover")) {
-    if (!length(hover)) "Hover on a point" else jsonlite::fromJSON(hover)
+    if (!length(hover)) "Hover on a point" else hover
   },
   output(id = 'hover')
 )
 
 app$callback_print(
   function(click = input("plotID", "input_plotly_click")) {
-    if (!length(click)) "Click on a point" else jsonlite::fromJSON(click)
+    if (!length(click)) "Click on a point" else click
   },
   output(id = 'click')
 )
 
 app$callback_print(
   function(select = input("plotID", "input_plotly_selected")) {
-    if (!length(select)) "Brush points" else jsonlite::fromJSON(select)
+    if (!length(select)) "Brush points" else select
   },
   output(id = 'select')
 )
 
 app$callback_print(
   function(bounds = input("plotID", "input_plotly_relayout")) {
-    if (!length(bounds)) "Zoom and pan" else jsonlite::fromJSON(bounds)
+    if (!length(bounds)) "Zoom and pan" else bounds
   },
   output(id = 'bounds')
 )
