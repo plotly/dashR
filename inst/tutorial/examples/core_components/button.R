aa83fc4ac90f4ac445e77652585494f04032d900
Nadia Tahiri <tahiri.nadia@courrier.uqam.ca>
2019-01-30 13:12:52 -0500

    First time check if list or not

--- inst/tutorial/examples/core_components/button.R
+++ inst/tutorial/examples/core_components/button.R
@@ -1,6 +1,7 @@
 library(dashR)
-
+  
 app <- Dash$new()
+
 app$layout_set(
   htmlDiv(coreInput(id = "input-box", type = "text")),
   htmlButton("submit", id = "button"),
@@ -9,12 +10,23 @@ app$layout_set(
 
 app$callback(
   function(n_clicks = input("button", "n_clicks"), value = input("input-box")) {
-    sprintf(
-      'The input value was "%s" and the button has been clicked %s times',
-      value, n_clicks
-    )
+    #print(length(value))
+    #print(value)
+    if (length(value)>1){
+      sprintf(
+        'The input value was "%s" and the button has been clicked %s times',
+        value[2], n_clicks
+      )
+    }else{
+      sprintf(
+        'The input value was "%s" and the button has been clicked %s times',
+        value, n_clicks
+      )
+    }
   },
   output("output-container-button")
 )
+
+
 
 app
