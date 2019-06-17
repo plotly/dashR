3145f5795ea9c4e81525e3341c3b4f02f8c66af9
Ryan Patrick Kyle <ryan@plot.ly>
2019-06-17 16:11:13 -0400

    export updated fn name

--- R/dash.R
+++ R/dash.R
@@ -946,7 +946,7 @@ validate_dependency <- function(layout_, dependency) {
   TRUE
 }
 
-#' @export assert_valid_wildcards
+#' @export dash_assert_valid_wildcards
 dash_assert_valid_wildcards <- function (...)
 {
   args <- list(...)
