a8d5e561d379f56ab5842f9d63f8f65bedca7e6f
Ryan Patrick Kyle <ryan@plot.ly>
2019-06-19 00:05:28 -0400

    :pencil2: use proper metadata fn

--- R/dash.R
+++ R/dash.R
@@ -758,9 +758,9 @@ Dash <- R6::R6Class(
     .index = NULL,
     
     collect_resources = function() {
-      # DashR's own dependencies
+      # Dash's own dependencies
       # serve the dev version of dash-renderer when in debug mode
-      dependencies_all_internal <- dashR:::.dashR_js_metadata()
+      dependencies_all_internal <- dash:::.dash_js_metadata()
       if (private$debug) {
         depsSubset <- dependencies_all_internal[names(dependencies_all_internal) != c("dash-renderer-prod",
                                                                                       "dash-renderer-map-prod")]
