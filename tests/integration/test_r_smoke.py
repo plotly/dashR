15df05874afdc652b1d34dbc2935f4f83314fe2d
byron <byron@plot.ly>
2019-07-23 11:33:27 -0400

    :robot: make it more stable

--- tests/integration/test_r_smoke.py
+++ tests/integration/test_r_smoke.py
@@ -10,5 +10,7 @@ app$run_server()
 
 def test_rstr001_r_with_string(dashr):
     dashr.start_server(app)
-    assert dashr.find_element("#container").text == "Hello Dash for R testing"
+    dashr.wait_for_text_to_equal(
+        "#container", "Hello Dash for R testing", timeout=1
+    )
     dashr.percy_snapshot("rstr001 - hello dash")
