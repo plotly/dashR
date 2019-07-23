a0866dc319cefab154cfac670b4bb44d0002c7ec
byron <byron@plot.ly>
2019-07-23 11:27:54 -0400

    :lipstick: polish test case

--- tests/integration/test_r_smoke.py
+++ tests/integration/test_r_smoke.py
@@ -1,12 +1,14 @@
-app = '''
+app = """
 library(dash)
 library(dashHtmlComponents)
 app <- Dash$new()
 
 app$layout(htmlDiv(list(htmlDiv(id='container',children='Hello Dash for R testing'))))
 app$run_server()
-'''
+"""
 
-def test_r001_r_with_string(dashr):
+
+def test_rstr001_r_with_string(dashr):
     dashr.start_server(app)
-    dashr.percy_snapshot("r001 - hello dash")
+    assert dashr.find_element("#container").text == "Hello Dash for R testing"
+    dashr.percy_snapshot("rstr001 - hello dash")
