83d35010e569c1af9cd476efbb4ca74b0ec23da3
Ryan Patrick Kyle <ryan@plot.ly>
2019-08-27 01:18:35 -0400

    :rotating_light: add test for partial outputs

--- tests/integration/callbacks/multiple_outputs.py
+++ tests/integration/callbacks/multiple_outputs.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.support.select import Select
-import time
 
 app = """
 library(dash)
@@ -9,7 +8,6 @@ library(plotly)
 library(dashTable)
 
 app <- Dash$new()       
-
 app$layout(
   htmlDiv(list(
     htmlDiv(list(
@@ -20,50 +18,60 @@ app$layout(
                          value = 'movies'),
                     list(label = 'Series',
                          value = 'series')
-                    ),
+                  ),
                   value = 'movies')
     ),
     id = 'container',
     style = list(
       backgroundColor = '#ff998a'
-      )
+    )
     ),
     htmlDiv(list(
       htmlH2('Make a selection from the dropdown menu.',
-             id = 'text-box')
+             id = 'text-box'),
+      dccRadioItems(id='radio-partial',
+                    options = list(
+                      list(label = 'All',
+                           value = 'all'),
+                      list(label = 'Do not update colour',
+                           value = 'static')
+                    ),
+                    value = 'all')
     )
     )
-    )
-    )
+  )
+  )
 )
-
 app$callback(output=list(
   output(id='text-box', property='children'),
   output(id='container', property='style')
 ),
 params=list(
-  input(id='data-dropdown', property='value')
+  input(id='data-dropdown', property='value'),
+  input(id='radio-partial', property='value')
 ),
-function(value) {
+function(value, choice) {
   if (is.null(value)) {
     return(dashNoUpdate())
   }
   
-  if (value == "series") {
+  if (choice == "all" && value == "series") {
     style <- list(
       backgroundColor = '#ff998a'
     )
-  } else {
+  } else if (choice == "all") {
     style <- list(
       backgroundColor = '#fff289'
     )
+  } else {
+    return(list(sprintf("You have chosen %s.", value),
+                dashNoUpdate()))
   }
   
   return(list(sprintf("You have chosen %s.", value),
               style))
 }
 )
-
 app$run_server(debug=TRUE)
 """
 
@@ -85,4 +93,20 @@ def test_rsnu001_multiple_outputs(dashr):
         "You have chosen movies."
     )
     backgroundColor = dashr.find_element('#container').value_of_css_property("background-color")
+    assert backgroundColor == "rgba(255, 242, 137, 1)"
+    dashr.find_elements("input[type='radio']")[1].click()
+    dashr.find_element("#data-dropdown").click()
+    dashr.find_elements("div.VirtualizedSelectOption")[1].click()
+    dashr.wait_for_text_to_equal(
+        "#text-box",
+        "You have chosen series."
+    )
+    assert backgroundColor == "rgba(255, 242, 137, 1)"
+    dashr.find_elements("input[type='radio']")[0].click()
+    dashr.find_element("#data-dropdown").click()
+    dashr.find_elements("div.VirtualizedSelectOption")[0].click()
+    dashr.wait_for_text_to_equal(
+        "#text-box",
+        "You have chosen movies."
+    )
     assert backgroundColor == "rgba(255, 242, 137, 1)"
