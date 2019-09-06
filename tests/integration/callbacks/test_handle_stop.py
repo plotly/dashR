1029ec48b4ee76c3c6b85ad999532876a98c5b38
Ryan Patrick Kyle <ryan@plot.ly>
2019-09-06 00:34:28 -0400

    :rotating_light: add test for stop errors

---
+++ tests/integration/callbacks/test_handle_stop.py
@@ -0,0 +1,46 @@
+from selenium.webdriver.support.select import Select
+
+app = """
+library(dash)
+library(dashHtmlComponents)
+library(dashCoreComponents)
+
+app <- Dash$new()
+
+app$layout(
+  htmlDiv(
+    list(
+      dccDropdown(options = list(
+        list(label = "Red", value = "#FF0000"),
+        list(label = "Throw error", value = "error")
+      ),
+      id = "input-choice",
+      value = "error"),
+      htmlDiv(id="div-choice")
+    )
+  )
+)
+
+app$callback(output(id = 'div-choice', property = 'children'),
+             list(input(id = 'input-choice', property = 'value')),
+             function(choice) {
+               if (choice == "error") {
+                 stop(simpleError("Throwing an error by request"))
+                 }
+               if (!is.null(unlist(choice))) {
+                 return(sprintf("Choice was %s", choice))
+                 } else {
+                   return(sprintf("Make a choice"))
+                   }
+               })
+
+app$run_server(debug=TRUE)
+"""
+
+
+def test_rshs001_handle_stop(dashr):
+    dashr.start_server(app)
+    dashr.wait_for_text_to_equal(
+        ".dash-fe-error__title",
+        "Callback error updating div-choice.children"
+    )
