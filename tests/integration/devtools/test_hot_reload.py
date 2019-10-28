f03418e98b843def88454a0e428d9f52bbbb7de0
Ryan Patrick Kyle <ryan@plot.ly>
2019-10-28 18:24:51 -0400

    fix hot reload test v2, callback

--- tests/integration/devtools/test_hot_reload.py
+++ tests/integration/devtools/test_hot_reload.py
@@ -12,13 +12,26 @@ RED_BG = """
 app = """
 library(dash)
 library(dashHtmlComponents)
+library(dashCoreComponents)
 app <- Dash$new()
 
 app$layout(htmlDiv(list(
-htmlH3("Hot reload")
+htmlH3("Hot reload"),
+dccInput(id='input'),
+htmlDiv(id='output-serverside')
 ),
 id="hot-reload-content"
 )
+)
+
+app$callback(
+  output(id = "output-serverside", property = "children"),
+  params = list(
+    input(id = "input", property = "value")
+  ),
+  function(value) {
+    sprintf("Test output should be %s", value)
+  }
 )
 
 app$run_server(dev_tools_hot_reload=TRUE, dev_tools_hot_reload_interval=0.1, dev_tools_silence_routes_logging=TRUE)
@@ -32,6 +45,13 @@ def test_rsdv001_hot_reload(dashr):
         "background-color",
         "rgba(0, 0, 255, 1)"
     )
+    dashr.wait_for_text_to_equal(
+        "#output-serverside",
+        "Test output should be NULL"
+    )
+    input1 = dashr.find_element("#input")
+    dashr.clear_input(input1)
+    input1.send_keys("unchanged")
     hot_reload_file = os.path.join(
         os.path.dirname(__file__), "assets", "hot_reload.css"
     )
@@ -45,6 +65,10 @@ def test_rsdv001_hot_reload(dashr):
         "#hot-reload-content",
         "background-color",
         "rgba(255, 0, 0, 1)"
+    )
+    dashr.wait_for_text_to_equal(
+        "#output-serverside",
+        "Test output should be unchanged"
     )
     with open(hot_reload_file, "w") as f:
         f.write(old_content)
