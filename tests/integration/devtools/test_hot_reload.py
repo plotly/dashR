27909ee10e1db73b4e12a3feda66d442f0cd993f
Ryan Patrick Kyle <ryan@plot.ly>
2019-10-28 18:03:00 -0400

    fix hot reload test v1

--- tests/integration/devtools/test_hot_reload.py
+++ tests/integration/devtools/test_hot_reload.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.support.select import Select
-import os
+import time, os
 
 
 RED_BG = """
@@ -11,9 +11,8 @@ RED_BG = """
 
 app = """
 library(dash)
-library(dashCoreComponents)
 library(dashHtmlComponents)
-app <- Dash$new(assets_folder="hr_assets")
+app <- Dash$new()
 
 app$layout(htmlDiv(list(
 htmlH3("Hot reload")
@@ -21,36 +20,37 @@ htmlH3("Hot reload")
 id="hot-reload-content"
 )
 )
+
 app$run_server(dev_tools_hot_reload=TRUE, dev_tools_hot_reload_interval=0.1, dev_tools_silence_routes_logging=TRUE)
 """
 
 
 def test_rsdv001_hot_reload(dashr):
-    os.chdir(os.path.dirname(__file__))
     dashr.start_server(app)
     dashr.wait_for_style_to_equal(
-      "#hot-reload-content", 
-      "background-color", 
-      "rgba(0, 0, 255, 1)"
-      )
+        "#hot-reload-content",
+        "background-color",
+        "rgba(0, 0, 255, 1)"
+    )
     hot_reload_file = os.path.join(
-      os.path.dirname(__file__), "hr_assets", "hot_reload.css"
-      )
+        os.path.dirname(__file__), "assets", "hot_reload.css"
+    )
     with open(hot_reload_file, "r+") as fp:
-      sleep(1)  # ensure a new mod time
-      old_content = fp.read()
-      fp.truncate(0)
-      fp.seek(0)
-      fp.write(RED_BG)
+        time.sleep(1)  # ensure a new mod time
+        old_content = fp.read()
+        fp.truncate(0)
+        fp.seek(0)
+        fp.write(RED_BG)
     dashr.wait_for_style_to_equal(
-      "#hot-reload-content", 
-      "background-color", 
-      "rgba(255, 0, 0, 1)"
-      )
+        "#hot-reload-content",
+        "background-color",
+        "rgba(255, 0, 0, 1)"
+    )
     with open(hot_reload_file, "w") as f:
-      f.write(old_content)
+        f.write(old_content)
+        time.sleep(1)
     dashr.wait_for_style_to_equal(
-      "#hot-reload-content", 
-      "background-color", 
-      "rgba(0, 0, 255, 1)"
-      )
+        "#hot-reload-content",
+        "background-color",
+        "rgba(0, 0, 255, 1)"
+    )
