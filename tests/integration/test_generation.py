40c62a51046e4b125f1e4049cfafc7c87396f78c
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2020-04-23 16:58:40 -0400

    - use DashPy arbitrary extension branch - test arbitrary extension + snapshots

--- tests/integration/test_generation.py
+++ tests/integration/test_generation.py
@@ -17,9 +17,32 @@ app$layout(htmlDiv(list(
 app$run_server()
 """
 
+styled_app = """
+library(dash)
+library(dashHtmlComponents)
+library(dashGeneratorTestComponentStandard)
+
+app <- Dash$new()
+app$layout(htmlDiv(list(
+    dgtc_standardMyStandardComponent(id="standard", value="Standard", style=list("font-family": "godfather")),
+)))
+
+app$run_server()
+"""
+
 
 def test_gene001_simple_callback(dashr):
     dashr.start_server(app)
 
     assert dashr.wait_for_element("#standard").text == "Standard"
     assert dashr.wait_for_element("#nested").text == "Nested"
+
+    dashr.percy_snapshot("gene001-simple-callback")
+
+
+def test_gene002_arbitrary_resources(dashr):
+    dashr.start_server(app)
+
+    assert dashr.wait_for_element("#standard").text == "Standard"
+
+    dashr.percy_snapshot("gene002-arbitrary-resource")
