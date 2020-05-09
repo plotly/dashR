0284551cf088ce7fc2a4b0f9e7b5a1ff98d585c9
Ryan Patrick Kyle <ryan@plot.ly>
2020-05-09 12:47:03 -0400

    :rotating_light: add DOPSA for build test

---
+++ tests/integration/dopsa/test_dopsa.py
@@ -0,0 +1,66 @@
+app = """
+library(dash)
+library(dashCoreComponents)
+library(dashHtmlComponents)
+
+app <- Dash$new()
+
+app$layout(
+  htmlDiv(
+    list(
+      dccInput(id='input-1-state', type='text', value='Montreal'),
+      dccInput(id='input-2-state', type='text', value='Canada'),
+      htmlButton(id='submit-button', n_clicks=0, children='Submit'),
+      htmlDiv(id='output-state'),
+      dccGraph(id='graph',
+        figure=list(
+          data=list(
+            list(
+              x=list(1, 2, 3),
+              y=list(4, 1, 2),
+              type='bar',
+              name='SF'
+            ),
+            list(
+              x=list(1, 2, 3),
+              y=list(2, 4, 5),
+              type='bar',
+              name='Montr\U{00E9}al'
+            )
+          ),
+          layout = list(title='Dash Data Visualization')
+        )
+      )
+    )
+  )
+)
+
+
+app$callback(output(id = 'output-state', property = 'children'),
+             list(input(id = 'submit-button', property = 'n_clicks'),
+                  state(id = 'input-1-state', property = 'value'),
+                  state(id = 'input-2-state', property = 'value')),
+             function(n_clicks, input1, input2) {
+               sprintf('The Button has been pressed \\'%s\\' times, Input 1 is \\'%s\\', and Input 2 is \\'%s\\'', n_clicks, input1, input2)
+             })
+
+app$run_server()
+"""
+
+
+def test_rsdp001_dopsa(dashr):
+    dashr.start_server(app)
+    dashr.wait_for_text_to_equal(
+        "#output-state", "The Button has been pressed '0' times, Input 1 is 'Montreal', and Input 2 is 'Canada'", timeout=1
+    )
+    input1 = dashr.find_element("#input-2-state")
+    dashr.clear_input(input1)
+    input1.send_keys("Quebec")
+    dashr.wait_for_text_to_equal(
+        "#input-2-state", "Quebec", timeout=1
+        ) 
+    dashr.find_element("#submit-button").click()
+    dashr.wait_for_text_to_equal(
+        "#output-state", "The Button has been pressed '1' times, Input 1 is 'Montreal', and Input 2 is 'Quebec'", timeout=1
+    )
+    dashr.percy_snapshot("rsdp001 - dopsa for R")
