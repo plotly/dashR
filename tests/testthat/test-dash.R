8233d35099bb5dbdb7d9d56b6771c62b01b97519
Ryan Patrick Kyle <ryan@plot.ly>
2020-05-25 14:28:37 -0400

    :hammer: :rotating_light: fix unit test

--- tests/testthat/test-dash.R
+++ tests/testthat/test-dash.R
@@ -12,11 +12,14 @@ test_that("Can set/get layout", {
 
   d <- Dash$new()
   div <- htmlDiv("A div", id = "An id")
-
+  d$layout(div)
+ 
+  l <- d$layout_get()
+ 
   # dynamic layouts
-  d$layout(function() { div })
+  d$layout(function() { htmlDiv("A div", id = "An id") })
   l2 <- d$layout_get()
-  # expect_identical(l, l2)
+  expect_identical(l, l2)
   expect_is(d$layout_get(render = FALSE), "function")
 
 })
