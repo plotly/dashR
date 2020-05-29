c0c25630877ad2d37177e5851312c2f1c7a2f743
Ryan Patrick Kyle <rpkyle@users.noreply.github.com>
2020-05-29 09:31:42 -0400

    Dash for R v0.5.0 (#205)
    * Add dopsa test for proposed R build test (#199)
    
    * Remove is.layout, layout_container_id, and redundant component test (#121)
    
    * Add brotli import (#204)

--- tests/testthat/test-dash.R
+++ tests/testthat/test-dash.R
@@ -12,15 +12,12 @@ test_that("Can set/get layout", {
 
   d <- Dash$new()
   div <- htmlDiv("A div", id = "An id")
-
-  # rendered layout has a container div
   d$layout(div)
+ 
   l <- d$layout_get()
-  expect_true(dash:::is.layout(l))
-  expect_identical(l$props$children[[1]], div)
-
+ 
   # dynamic layouts
-  d$layout(function() { div })
+  d$layout(function() { htmlDiv("A div", id = "An id") })
   l2 <- d$layout_get()
   expect_identical(l, l2)
   expect_is(d$layout_get(render = FALSE), "function")
