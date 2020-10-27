b477fe0aa8965c5477abce2177e250fdbde17a98
rpkyle <ryan@plotly.com>
2020-10-27 01:46:56 -0400

    add PMC example

--- R/dash.R
+++ R/dash.R
@@ -829,6 +829,60 @@ Dash <- R6::R6Class(
     #' on a key that has `MATCH` in the `output` item(s). `ALLSMALLER` it isn't always
     #' necessary (you can usually use `ALL` and filter out the indices in your callback),
     #' but it will make your logic simpler.
+    #' @examples
+    #' if (interactive() && require(dash)) {
+    #'   library(dashCoreComponents)
+    #'   library(dashHtmlComponents)
+    #'   library(dash)
+    #'
+    #'   app <- Dash$new()
+    #'
+    #'   app$layout(htmlDiv(list(
+    #'     htmlButton("Add Filter", id="add-filter", n_clicks=0),
+    #'     htmlDiv(id="dropdown-container", children=list()),
+    #'     htmlDiv(id="dropdown-container-output")
+    #'   )))
+    #'
+    #'
+    #'   app$callback(
+    #'     output(id="dropdown-container", property = "children"),
+    #'     params = list(
+    #'       input(id = "add-filter", property = "n_clicks"),
+    #'       state(id = "dropdown-container", property = "children")
+    #'     ),
+    #'     display_dropdowns <- function(n_clicks, children){
+    #'       new_dropdown = dccDropdown(
+    #'         id=list(
+    #'           "index" = n_clicks,
+    #'           "type" = "filter-dropdown"
+    #'         ),
+    #'         options = lapply(c("NYC", "MTL", "LA", "TOKYO"), function(x){
+    #'           list("label" = x, "value" = x)
+    #'         })
+    #'       )
+    #'       children[[n_clicks + 1]] <- new_dropdown
+    #'       return(children)
+    #'     }
+    #'   )
+    #'
+    #'   app$callback(
+    #'     output(id="dropdown-container-output", property="children"),
+    #'     params = list(
+    #'       input(id=list("index" = ALL, "type" = "filter-dropdown"), property= "value")
+    #'     ),
+    #'     display_output <- function(test){
+    #'       ctx <- app$callback_context()
+    #'       return(htmlDiv(
+    #'         lapply(seq_along(test), function(x){
+    #'           return(htmlDiv(sprintf("Dropdown %s = %s", x, test[[x]])))
+    #'         })
+    #'       ))
+    #'     }
+    #'   )
+    #'
+    #'   app$run_server()
+    #'}
+    #'
     callback = function(output, params, func) {
       assert_valid_callbacks(output, params, func)
       inputs <- params[vapply(params, function(x) 'input' %in% attr(x, "class"), FUN.VALUE=logical(1))]
